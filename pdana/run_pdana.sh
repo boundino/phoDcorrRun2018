ad9a62d35afd04b9c599c230e1d5bb5f554594c9
Jing <boundino@gmail.com>
2020-09-26 23:24:54 -0400

    .sh run multiple configs

--- pdana/run_pdana.sh
+++ pdana/run_pdana.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-ptmin=2
-ptmax=5
-ymax=1.2
+ikine=(0 1 2 3)
+
+#
+ptmin=(2 3 4 5)
+ptmax=(100 100 100 100)
+ymax=(2 2 2 2)
+
 centmin=0
 centmax=90
 
-tag="prel"
-# tag="prel_finebin"
+# tag="prel"
+tag="prel_finebin"
 
-input=/raid5/data/wangj/DntupleRun2018/phod_20200923_crab_Dpho_20200921_HIHardProbes_04Apr2019_HIGEDPhoton40_trk1Dpt2.root
-
-##
-g++ getfname.cc -I"../includes/" $(root-config --libs --cflags) -g -o getfname_${tmp}.exe || { rm *_${tmp}.exe 2> /dev/null ; exit 1 ; }
-tagki=$(./getfname_${tmp}.exe $ptmin $ptmax $ymax $centmin $centmax)
-rm getfname_${tmp}.exe
+input=/raid5/data/wangj/DntupleRun2018/phodmva_20200924_crab_Dpho_20200921_HIHardProbes_04Apr2019_HIGEDPhoton40_trk1Dpt2.root
+ishi=1
 
 ##
 run_savehist=${1:-0}
 run_fithist=${2:-0}
 
+##
+g++ getfname.cc -I"../includes/" $(root-config --libs --cflags) -g -o getfname.exe || { rm *.exe 2> /dev/null ; exit 1 ; }
 [[ $run_savehist -eq 1 || $# == 0 ]] && { g++ pdana_savehist.cc $(root-config --libs --cflags) -I"../includes/" -g -o pdana_savehist.exe || exit 1 ; }
 [[ $run_fithist -eq 1 || $# == 0 ]] && { g++ pdana_fithist.cc $(root-config --libs --cflags) -I"../includes/" -g -o pdana_fithist.exe || exit 1 ; }
 
-[[ $run_savehist -eq 1 ]] && { ./pdana_savehist.exe $input $tag $ptmin $ptmax $ymax $centmin $centmax ; }
-input_fithist=rootfiles/${tag}_${tagki}/savehist.root
-set -x
-[[ $run_fithist -eq 1 ]] && { ./pdana_fithist.exe $input_fithist $tag ; }
-set +x
+for i in ${ikine[@]}
+do
+    tagki=$(./getfname.exe $ishi ${ptmin[i]} ${ptmax[i]} ${ymax[i]} $centmin $centmax)
 
+    [[ $run_savehist -eq 1 ]] && { ./pdana_savehist.exe $input $tag $ishi ${ptmin[i]} ${ptmax[i]} ${ymax[i]} $centmin $centmax ; }
+    input_fithist=rootfiles/${tag}_${tagki}/savehist.root
+    [[ $run_fithist -eq 1 ]] && { ./pdana_fithist.exe $input_fithist $tag ; }
+done
+
+rm getfname.exe
 [[ -f pdana_savehist.exe ]] && rm pdana_savehist.exe
 [[ -f pdana_fithist.exe ]] && rm pdana_fithist.exe
 
