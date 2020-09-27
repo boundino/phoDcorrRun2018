1e0e9d867f3762ef4fe23287deb563d57e226870
Jing <boundino@gmail.com>
2020-09-26 23:16:00 -0400

    add ishi in param

--- includes/getfname.cc
+++ includes/getfname.cc
@@ -3,9 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc==6)
+  if(argc==7)
     { 
-      phoD::param pa(atof(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), "Q");
+      phoD::param pa(atoi(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]), "Q");
       std::cout<<pa.tag()<<std::endl;
       return 0; 
     }
