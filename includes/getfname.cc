bd0d686b80d9402281b8fe634c19369616fefe02
Jing <boundino@gmail.com>
2020-10-05 17:06:49 -0400

    add photon pt eta in config

--- includes/getfname.cc
+++ includes/getfname.cc
@@ -3,9 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc==7)
+  if(argc==9)
     { 
-      phoD::param pa(atoi(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]), "Q");
+      phoD::param pa(atoi(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]), atof(argv[8]), "Q");
       std::cout<<pa.tag()<<std::endl;
       return 0; 
     }
