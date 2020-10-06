13c1f7dc9bbc3515b31ffc380525e538f3ab0855
Jing <boundino@gmail.com>
2020-10-06 01:21:26 -0400

    update .sh

--- pdana/run_comp.sh
+++ pdana/run_comp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ikine=({0..1})
+ikine=(1 2)
 
 # rootfiles/prel_finebin_pp_phopt40_pt5-100_y1.2/calchist.root
 config=(
     "'rootfiles/prel_finebin_pp_*_pt5-100_y1.2/calchist.root' phopt40,phopt50,phopt60,phopt80,phopt100 prel_finebin_pp_pt5-100_y1.2 phopt histe"
-    "'rootfiles/prel_finebin_pp_phopt40_*_y1.2/calchist.root' pt2-100,pt2-5 prel_finebin_pp_phopt40_y1.2 pt pe"
+    "'rootfiles/prel_finebin_pp_phopt40_*_y1.2/calchist.root' pt2-100,pt2-5 prel_finebin_pp_phopt40_y1.2 pt histe"
+    "'rootfiles/prel_finebin_PbPb_phopt40_*_y1.2_cent090/calchist.root' pt2-100,pt2-5 prel_finebin_PbPb_phopt40_y1.2_cent090 pt histe"
 )
 
 ##
