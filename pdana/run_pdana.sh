15c60addb37f3979f486171c0dcf75bbab6d9ed6
Jing <boundino@gmail.com>
2020-10-05 23:48:52 -0400

    check input file before run

--- pdana/run_pdana.sh
+++ pdana/run_pdana.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ikine=(0)
-jsyst=(1)
+jsyst=(0) # ishi
 
 #
 # Dptmin Dptmax Dymax centmin centmax phoptmin phoetamax
 config=(
     "2 100 1.2 0 90 40 1.442"
+    "2 5 1.2 0 90 40 1.442"
     "5 100 1.2 0 90 40 1.442"
     "5 100 1.2 0 90 50 1.442"
     "5 100 1.2 0 90 60 1.442"
@@ -18,10 +19,10 @@ config=(
 tag="prel_finebin"
 
 input=(
-    /raid5/data/wangj/DntupleRun2018/phodmva_20200924_Dpho_20200921_HIHardProbes_04Apr2019_HIGEDPhoton40_trk1Dpt2.root
     /raid5/data/wangj/DntupleRun2017/phodcut_20201002_Dpho_20201002_HighEGJet_17Nov2017_HIPhoton_HoverELoose_trk1Dpt2_vfl2.root
+    /raid5/data/wangj/DntupleRun2018/phodmva_20200924_Dpho_20200921_HIHardProbes_04Apr2019_HIGEDPhoton40_trk1Dpt2.root
 )
-ishi=(1 0)
+ishi=(0 1)
 
 ##
 run_savehist=${1:-0}
@@ -43,20 +44,21 @@ do
         [[ $run_savehist -eq 1 ]] && { ./pdana_savehist.exe ${input[j]} $tag ${ishi[j]} ${config[i]} ; }
 
         input_fithist=rootfiles/${tag}_${tagki}/savehist.root
-        [[ -f $input_fithist ]] && {
-            [[ $run_fithist -eq 1 ]] && { ./pdana_fithist.exe $input_fithist $tag ; } } || {
-            echo -e "\e[31;1merror:\e[0m no input file \e[4m$input_fithist\e[0m." ; }
+        [[ $run_fithist -eq 1 ]] && {
+            [[ -f $input_fithist ]] && 
+            { ./pdana_fithist.exe $input_fithist $tag ; } ||
+            { echo -e "\e[31;1merror:\e[0m no input file \e[4m$input_fithist\e[0m." ; } }
 
         input_calchist=rootfiles/${tag}_${tagki}/fithist.root
-        [[ -f $input_calchist ]] && {
-            [[ $run_calchist -eq 1 ]] && { ./pdana_calchist.exe $input_calchist $tag ; } } || {
-            echo -e "\e[31;1merror:\e[0m no input file \e[4m$input_calchist\e[0m." ; }
+        [[ $run_calchist -eq 1 ]] && {
+            [[ -f $input_calchist ]] && 
+            { ./pdana_calchist.exe $input_calchist $tag ; } ||
+            { echo -e "\e[31;1merror:\e[0m no input file \e[4m$input_calchist\e[0m." ; } }
 
     done
 done
 
-rm getfname.exe
-[[ -f pdana_savehist.exe ]] && rm pdana_savehist.exe
-[[ -f pdana_fithist.exe ]] && rm pdana_fithist.exe
-[[ -f pdana_calchist.exe ]] && rm pdana_calchist.exe
-
+rm getfname.exe > /dev/null 2>&1
+rm pdana_savehist.exe > /dev/null 2>&1
+rm pdana_fithist.exe > /dev/null 2>&1
+rm pdana_calchist.exe > /dev/null 2>&1
