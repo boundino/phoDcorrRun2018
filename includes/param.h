8d0232e8fe9072447486469ea12ad033625237c8
Jing <boundino@gmail.com>
2020-10-05 23:49:35 -0400

    add macros to compare diff configs

--- includes/param.h
+++ includes/param.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include <iomanip>
 
 #include <TFile.h>
 #include <TTree.h>
@@ -14,9 +15,8 @@
 
 namespace phoD
 {
-  // std::vector<float> bins_dphi = {0, 0.2, 0.4, 0.6, 0.8, 1.0};
-  std::vector<float> bins_dphi = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
-  int nbins_dphi = bins_dphi.size() - 1;
+  std::vector<float> bins_dphi_aa = {0, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+  std::vector<float> bins_dphi_pp = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
 
   // photon selection
   const float hovere_max_pp_ = 0.009732;
@@ -47,7 +47,7 @@ namespace phoD
     bool ishi() { return (bool)ishi_; }
     std::string tag(std::string var = "") { return tag_[var]; }
     void print();
-    void drawtex(float xleft, float ytop, float tsize);
+    void drawtex(float xleft, float ytop, float tsize, std::string exlude="");
   private:
     int ishi_;
     std::map<std::string, float> val_;
@@ -90,7 +90,7 @@ phoD::param::param(int isheavyion, float Dptmin, float Dptmax, float Dymax,
 
 phoD::param::param(TFile* inf, std::string opt) : opt_(opt)
 {
-  if(opt_!="Q") std::cout<<"\e[32;1m -- "<<__PRETTY_FUNCTION__<<std::endl;
+  if(opt_!="Q") std::cout<<"\e[32;1m -- "<<__PRETTY_FUNCTION__<<"\e[0m"<<std::endl;
   TTree* p = (TTree*)inf->Get("param");
   p->SetBranchAddress("ishi", &ishi_);
   for(auto pp : list_) p->SetBranchAddress(pp.c_str(), &(val_[pp]));
@@ -143,14 +143,22 @@ void phoD::param::print()
             << std::left << std::setw(len) << "  "+tag_[""] << "" << std::endl << std::string(len, '-') << "\e[0m" << std::endl;
 }
 
-void phoD::param::drawtex(float xleft, float ytop, float tsize)
+void phoD::param::drawtex(float xleft, float ytop, float tsize, std::string exlude)
 {
+  std::vector<std::string> ex = xjjc::str_divide(exlude, ",");
   float ls = tsize*1.4;
-  xjjroot::drawtex(xleft, ytop, tag_["pt"].c_str(), tsize, 13);
-  xjjroot::drawtex(xleft, ytop-=ls, tag_["y"].c_str(), tsize, 13);
-  xjjroot::drawtex(xleft, ytop-=ls, tag_["phopt"].c_str(), tsize, 13);
-  xjjroot::drawtex(xleft, ytop-=ls, tag_["phoeta"].c_str(), tsize, 13);
-  xjjroot::drawtex(xleft, ytop-=ls, tag_["cent"].c_str(), tsize, 13);
+
+  float ytop_ = ytop + ls;
+  std::vector<std::string> info_left = {"pt", "y", "phopt", "phoeta"};
+  for(auto& i : info_left)
+    if(std::find(ex.begin(), ex.end(), i) == ex.end())
+      { xjjroot::drawtex(xleft, ytop_-=ls, tag_[i].c_str(), tsize, 13); }
+
+  std::vector<std::string> info_right = {"cent"};
+  ytop_ = ytop + ls;
+  for(auto& i : info_right)
+    if(std::find(ex.begin(), ex.end(), i) == ex.end())
+      { xjjroot::drawtex(xleft+0.5, ytop_-=ls, tag_[i].c_str(), tsize, 13); }
 }
 
 #endif
---
+++ pdana/run_comp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ikine=({0..1})
+
+# rootfiles/prel_finebin_pp_phopt40_pt5-100_y1.2/calchist.root
+config=(
+    "'rootfiles/prel_finebin_pp_*_pt5-100_y1.2/calchist.root' phopt40,phopt50,phopt60,phopt80,phopt100 prel_finebin_pp_pt5-100_y1.2 phopt histe"
+    "'rootfiles/prel_finebin_pp_phopt40_*_y1.2/calchist.root' pt2-100,pt2-5 prel_finebin_pp_phopt40_y1.2 pt pe"
+)
+
+##
+run_comphist=${1:-0}
+
+##
+[[ $run_comphist -eq 1 || $# == 0 ]] && { g++ comphist.cc $(root-config --libs --cflags) -I"../includes/" -g -o comphist.exe || exit 1 ; }
+
+for i in ${ikine[@]}
+do
+    [[ $run_comphist -eq 1 ]] && { ./comphist.exe ${config[i]} ; }
+done
+
+rm comphist.exe > /dev/null 2>&1
