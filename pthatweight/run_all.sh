f62cf6a092177dd59471a67598059a1c32fcd8c5
Jing <boundino@gmail.com>
2020-10-12 00:07:35 -0400

    new input for pthatweight

--- pthatweight/run_all.sh
+++ pthatweight/run_all.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-ichan=(0)
+ichan=(1)
 ###
 filelists=(
-    "/raid5/data/wangj/DntupleRun2018/phodmva_20200926_Dpho_20200924_QCDPhoton_pThat_*_Filter30GeV_TuneCP5_HydjetDrumMB_trk1Dpt2.root" # 0: QCDphoton + 30GeV filter
+    "/raid5/data/wangj/DntupleRun2018/phodmva_20200926_Dpho_20200924_QCDPhoton_pThat_*_Filter30GeV_TuneCP5_HydjetDrumMB_trk1Dpt2.root" # 0: QCDphoton + 30GeV filter (mva)
+    "/raid5/data/wangj/DntupleRun2018/phodgmatch_20201011_Dpho_20200924_QCDPhoton_pThat_*_Filter30GeV_TuneCP5_HydjetDrumMB_trk1Dpt2.root" # 0: QCDphoton + 30GeV filter (gen-match)
 )
 ##
 crosssec=(
+    'const int nBins=4; float pthatBin[nBins]={30, 50, 80, 120}; float crosssec[nBins+1]={1002508., 341974., 88321., 21579., 0.};'
     'const int nBins=4; float pthatBin[nBins]={30, 50, 80, 120}; float crosssec[nBins+1]={1002508., 341974., 88321., 21579., 0.};'
 )
 
