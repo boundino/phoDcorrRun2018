0c1b67a92e1198ffb2cf82602040e1eefa44a1b4
Jing Wang <boundino@gmail.com>
2021-04-06 16:30:45 -0400

    add dphi -pi/2 ~ 3pi/2

--- includes/utili.h
+++ includes/utili.h
@@ -7,6 +7,8 @@ namespace xjjana
 {
   float cal_dphi_0pi(float phi1, float phi2);
   float cal_dphi_01(float phi1, float phi2);
+  float cal_dphi_2pi(float phi1, float phi2);
+  float cal_dphi_2(float phi1, float phi2);
   float cal_dr(float phi1, float eta1, float phi2, float eta2);
 }
 
@@ -20,9 +22,27 @@ float xjjana::cal_dphi_0pi(float phi1, float phi2)
   return dphi;
 }
 
+float xjjana::cal_dphi_2pi(float phi1, float phi2)
+{
+  // phi1, phi2: -pi ~ pi
+  float dphi = phi1 - phi2; // -2pi ~ 2pi
+  if(dphi < -M_PI/2.) dphi += 2*M_PI;
+  if(dphi > M_PI*3/2.) dphi -= 2*M_PI; // -pi/2 ~ 3pi/2
+
+  return dphi;
+}
+
 float xjjana::cal_dphi_01(float phi1, float phi2)
 {
   float dphi = cal_dphi_0pi(phi1, phi2);
+  dphi /= M_PI;
+
+  return dphi;
+}
+
+float xjjana::cal_dphi_2(float phi1, float phi2)
+{
+  float dphi = cal_dphi_2pi(phi1, phi2);
   dphi /= M_PI;
 
   return dphi;
